1509c0132265809f031248b7713b555666cea89b
raktim <raktim.banerjee@codeclouds.in>
2023-02-16 15:28:55 +0530

    first commit

---
+++ src/automation/TestDemo2.java
@@ -0,0 +1,24 @@
+package automation;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestDemo2 {
+	
+	@Test
+	public void titleCheckGoogle() throws MalformedURLException 
+	{
+		MutableCapabilities caps = new MutableCapabilities();
+		
+		WebDriver driver =  new RemoteWebDriver(new URL("https://hub.browserstack.com/wd/hub"), caps);
+		driver.get("https://www.google.com/");
+		Assert.assertTrue(driver.getTitle().matches("Google"));
+	}
+	
+
+}
